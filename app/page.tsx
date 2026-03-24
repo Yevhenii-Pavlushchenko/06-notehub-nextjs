ef7b46109f0f97907a6eca2c26d824c2e3eac0d2
JustFun <paw93@ukr.net>
2026-03-24 21:15:27 +0200

    corrected the mentor's comments
    1)поменял типизацию tag
    2)Передал путь к стилям в АПП
    3)Инициализировал  queryClient через useState

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 
-import css from ".//Home.module.css";
+import css from "./Home.module.css";
 
 export default function Home() {
   return (
--- components/TanStackProvider/TanStackProvider.tsx
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -1,13 +1,14 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 interface TanstackProviderProps { 
     children: React.ReactNode;
 }
 
 export default function TanstackProvider({ children }: TanstackProviderProps) {
-    const queryClient = new QueryClient();
+    const [queryClient] = useState(() => new QueryClient());
 
     return (
         <QueryClientProvider client={queryClient}>
